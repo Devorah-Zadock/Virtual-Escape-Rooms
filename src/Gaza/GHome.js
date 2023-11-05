67e8fb365ab8830079824f5fdbda8d421cc83bb5
Devorah Zadock <devorahzadock@gmail.com>
2023-11-06 00:51:51 +0200

    updateSite2

--- src/Gaza/GHome.js
+++ src/Gaza/GHome.js
@@ -3,21 +3,45 @@ import './GHome.css';
 import { Link } from 'react-router-dom';
 import Stopwatch from '../components/Stopwatch';
 import Missile from '../components/Missile';
-import GazaMap from '../images/GazaMap3.png';
-
-{/* <p>!!!!!לא ניתן לברוח<br/>
-      !!!...חחח... עבדנו עליכם<br/><br/>
-      ....עוד מעט הטיל נוחת עליכם... ספירה לאחור
-      </p> */}
+import Puzzle1 from '../images/puzzle1.png';
+import Puzzle from '../components/Puzzle';
 
 const GHome = () => {
+  const openPuzzleWindow = () => {
+    const width = 600;
+    const height = 400;
+    const left = (window.innerWidth - width) / 2;
+    const top = (window.innerHeight - height) / 2;
+
+    const windowFeatures = `width=${width},height=${height},left=${left},top=${top}`;
+
+    const puzzleWindow = window.open('', 'Puzzle Window', windowFeatures);
+    puzzleWindow.document.title = 'Puzzle Window';
+    puzzleWindow.document.body.innerHTML = '<div id="puzzle-container">' +
+      '<h1>שאלה</h1>' +
+      '<img src={Puzzle1} id="puzzle"></img>' +
+      '<input type="password" id="userInput" />' +
+      '<button onclick="checkUserInput()">OK</button>' +
+      '</div>';
+
+    puzzleWindow.checkUserInput = function () {
+      const userInput = puzzleWindow.document.getElementById('userInput').value;
+      if (userInput === "1234")
+        puzzleWindow.close();
+      else {
+        const userInputField = puzzleWindow.document.getElementById('userInput').value = '';
+      }
+    };
+  };
+
+
   return (
     <div className='GHome-room'>
       <Stopwatch />
       <div id='GazaMap' className="GazaMap-container">
-        <Link to="/Egypt" className="GbuttonStyle" id='Erez'>Check point Erez</Link>
-        <Link to="/GazaPort" className="GbuttonStyle" id='GazaPort'>ميناء غزة البحري - الصيادين</Link>
-        <Link to="/Egypt" className="GbuttonStyle" id='Rafiah'>رفيع</Link>
+        <button className="GbuttonStyle" id='Erez'>Check point Erez</button>
+        <button className="GbuttonStyle" id='GazaPort' onClick={openPuzzleWindow}>ميناء غزة البحري - الصيادين</button>
+        <button className="GbuttonStyle" id='Rafiah'>رفيع</button>
       </div>
 
     </div>
---
+++ src/components/Puzzle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Puzzle = ({ onClose }) => {
+  return (
+    <div className="puzzle-container">
+      <p>היייייייייייייי</p>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+}
+
+export default Puzzle;
